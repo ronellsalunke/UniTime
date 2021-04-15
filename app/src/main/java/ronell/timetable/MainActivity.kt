7ccd24fb24be1ecd12c98887da3438e13f027b93
Ronell Salunke <ronell1292k@gmail.com>
2021-04-15 19:35:32 +0530

    Initial commit

---
+++ app/src/main/java/ronell/timetable/MainActivity.kt
@@ -0,0 +1,38 @@
+package ronell.timetable
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import ronell.timetable.ui.theme.UniTimeTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UniTimeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UniTimeTheme {
+        Greeting("Android")
+    }
+}
