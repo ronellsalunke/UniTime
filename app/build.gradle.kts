6876d3332acde7d2beff32e2b174b1cad3b31312
Ronell Salunke <ronell1292k@gmail.com>
2021-07-09 20:55:00 +0530

    fix(buildscript): Update dependencies: AGP 7.0.0-beta05 Compose 1.0.0-rc01 Core 1.6.0 MDC 1.4.0 Compose Activity 1.3.0-rc01 Espresso Core 3.4.0
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,21 +39,20 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = rootProject.extra["compose_version"] as String
-        kotlinCompilerVersion = "1.5.10"
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
-    implementation("com.google.android.material:material:1.3.0")
+    implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-beta02")
+    implementation("androidx.activity:activity-compose:1.3.0-rc01")
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
 }
