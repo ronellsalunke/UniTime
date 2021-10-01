0928bff82ac494392b464bbe043b3060ed7a1893
Ronell Salunke <ronell1292k@gmail.com>
2021-10-01 16:12:18 +0530

    chore: bump accompanist (0.18.0) and appcompat (1.3.1)
    Signed-off-by: Ronell Salunke <ronell1292k@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,14 +44,14 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.6.0")
-    implementation("androidx.appcompat:appcompat:1.3.0")
+    implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.compose.ui:ui:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.material:material:${rootProject.extra["compose_version"]}")
     implementation("androidx.compose.ui:ui-tooling:${rootProject.extra["compose_version"]}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.1")
-    implementation("com.google.accompanist:accompanist-systemuicontroller:0.15.0")
-    implementation("com.google.accompanist:accompanist-insets:0.16.0")
-    implementation("com.google.accompanist:accompanist-insets-ui:0.16.0")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.18.0")
+    implementation("com.google.accompanist:accompanist-insets:0.18.0")
+    implementation("com.google.accompanist:accompanist-insets-ui:0.18.0")
 }
